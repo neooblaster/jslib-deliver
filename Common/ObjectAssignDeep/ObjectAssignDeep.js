21a845be999669370308f32df28f32a2e9ae13e3
DUPRE Nicolas <neo-blaster@hotmail.fr>
2024-04-10 11:58:06 +0200

    Create ObjectAssignDeep.js

---
+++ Common/ObjectAssignDeep/ObjectAssignDeep.js
@@ -0,0 +1,127 @@
+/**
+ * Extend Object to offer a assignDeep method
+ *
+ * Author        : Nicolas DUPRE
+ * Sources       : object-assign-deep
+ * URL           : https://www.npmjs.com/package/object-assign-deep
+ * Orig. Version : v0.4.0
+ *
+ * Comment : I simply rearrange methods & implementation to extend Object
+ */
+function ObjectAssignDeep (){
+    let self = this;
+
+    self.cloneValue = function (value) {
+        // The value is an object so lets clone it.
+        if (self.getTypeOf(value) === 'object') {
+            return self.quickCloneObject(value);
+        }
+
+        // The value is an array so lets clone it.
+        else if (self.getTypeOf(value) === 'array') {
+            return self.quickCloneArray(value);
+        }
+
+        // Any other value can just be copied.
+        return value;
+    };
+
+    self.quickCloneArray = function (input) {
+        return input.map(self.cloneValue);
+    };
+
+    self.quickCloneObject = function (input) {
+        const output = {};
+
+        for (const key in input) {
+            if (!input.hasOwnProperty(key)) { continue; }
+
+            output[key] = self.cloneValue(input[key]);
+        }
+
+        return output;
+    };
+
+    self.getTypeOf = function (input) {
+        if (input === null) {
+            return 'null';
+        }
+
+        else if (typeof input === 'undefined') {
+            return 'undefined';
+        }
+
+        else if (typeof input === 'object') {
+            return (Array.isArray(input) ? 'array' : 'object');
+        }
+
+        return typeof input;
+    };
+
+    self.executeDeepMerge = function (target, _objects = [], _options = {}) {
+        const options = {
+            arrayBehaviour: _options.arrayBehaviour || 'replace',  // Can be "merge" or "replace".
+        };
+
+        // Ensure we have actual objects for each.
+        const objects = _objects.map(object => object || {});
+        const output = target || {};
+
+        // Enumerate the objects and their keys.
+        for (let oindex = 0; oindex < objects.length; oindex++) {
+            const object = objects[oindex];
+            const keys = Object.keys(object);
+
+            for (let kindex = 0; kindex < keys.length; kindex++) {
+                const key = keys[kindex];
+                const value = object[key];
+                const type = self.getTypeOf(value);
+                const existingValueType = self.getTypeOf(output[key]);
+
+                if (type === 'object') {
+                    if (existingValueType !== 'undefined') {
+                        const existingValue = (existingValueType === 'object' ? output[key] : {});
+                        output[key] = self.executeDeepMerge({}, [existingValue, self.quickCloneObject(value)], options);
+                    }
+                    else {
+                        output[key] = self.quickCloneObject(value);
+                    }
+                }
+
+                else if (type === 'array') {
+                    if (existingValueType === 'array') {
+                        const newValue = self.quickCloneArray(value);
+                        output[key] = (options.arrayBehaviour === 'merge' ? output[key].concat(newValue) : newValue);
+                    }
+                    else {
+                        output[key] = self.quickCloneArray(value);
+                    }
+                }
+
+                else {
+                    output[key] = value;
+                }
+
+            }
+        }
+
+        return output;
+    };
+
+    self.objectAssignDeep = function($target, ...$objects){
+        return self.executeDeepMerge($target, $objects);
+    };
+
+    return self.objectAssignDeep;
+}
+
+// Using .prototype will break component loading...
+// I suppose it's due to enumerate the new property
+// By defineProperties, we set the new method as no unemrable
+Object.defineProperties(Object, {
+  assignDeep: {
+    enumerable: false,
+    writable: false,
+    value: new ObjectAssignDeep()
+  }
+});
